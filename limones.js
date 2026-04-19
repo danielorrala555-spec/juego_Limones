c8d3e7db54258a8bd5150bc87bbdfea94f9d7c6c
danielorrala555-spce <danielorrala555@gmail.com>
2026-04-18 19:46:50 -0500

    mover izquierda

--- limones.js
+++ limones.js
@@ -3,6 +3,7 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+let personajeX=canvas.width/2
 
 function iniciarJuego(){
     dibujarSuelo();
@@ -16,5 +17,19 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="orange"
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),40,ALTURA_PERSONAJE)
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),40,ALTURA_PERSONAJE)
+}
+function moverIsquierda(){
+    personajeX=personajeX-10;
+    catualizarPantalla();
+}
+function catualizarPantalla(){
+    lipiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+
+}
+
+function lipiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
 }
