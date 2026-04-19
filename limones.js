d584cb7adc53151c00b0d5b6b5771b5127667db9
danielorrala555-spce <danielorrala555@gmail.com>
2026-04-18 19:20:50 -0500

    agregamos funcion agregarPersonaje, agregarSuelo

--- limones.js
+++ limones.js
@@ -1,3 +1,20 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
- 
+const ALTURA_SUELO=20;
+const ALTURA_PERSONAJE=60;
+const ANCHO_PERSONAJE=40;
+
+function iniciarJuego(){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function dibujarSuelo(){
+    ctx.fillStyle="blue"
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO)
+}
+
+function dibujarPersonaje(){
+    ctx.fillStyle="orange"
+    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),40,ALTURA_PERSONAJE)
+}
